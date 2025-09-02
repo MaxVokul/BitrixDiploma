ade4033a6f15609878ad3285d586bb7a017f7806
MaxVokul <keny2880@gmail.com>
2025-09-02 21:30:16 +0300

    компонент NEWS

--- index.php
+++ index.php
@@ -1,141 +1,289 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новости");?>
-
-
-    <section class="tssection">
-        <h2 class="mains">Top stories</h2>
-        <a href="topstory.html">
-            <div class="tssqr">
-                <img class="imgts" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/image 1.png" alt="">
-                <div class="tstext">
-                    <h3>The one question from Donald Trump that could sway many American voters in swing states.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-                        massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
-                        quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
-                    <p>2 hours ago / by Sfsafa</p>
-                </div>
-            </div>
-        </a>
-        <div class="tssqr">
-            <img class="imgts" id="tssqimg" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/image 2.png" alt="">
-            <div class="tstext">
-                <h3>Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.</h3>
-                <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper
-                    nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-                    enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus
-                    varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. </p>
-                <p>10 hours ago / by Najdsksd</p>
-            </div>
-        </div>
-        <div class="tssqr" id="tssqr">
-            <img class="imgts" id="tssqimg" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/image 3.png" alt="">
-            <div class="tstext">
-                <h3>Maecenas nec odio et ante tincidunt tempus.</h3>
-                <p>Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus
-                    tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat,
-                    leo eget bibendum sodales, augue velit cursus nunc.</p>
-                <p>24 hours ago / by Hskdmak</p>
-            </div>
-        </div>
-    </section>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news", 
+	"Top_Stories", 
+	array(
+		"ADD_ELEMENT_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
+		"DETAIL_DISPLAY_TOP_PAGER" => "N",
+		"DETAIL_FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"DETAIL_PAGER_SHOW_ALL" => "Y",
+		"DETAIL_PAGER_TEMPLATE" => "",
+		"DETAIL_PAGER_TITLE" => "Страница",
+		"DETAIL_PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"DETAIL_SET_CANONICAL_URL" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "7",
+		"IBLOCK_TYPE" => "news",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"LIST_FIELD_CODE" => array(
+			0 => "CREATED_BY",
+			1 => "CREATED_USER_NAME",
+			2 => "MODIFIED_BY",
+			3 => "USER_NAME",
+			4 => "",
+		),
+		"LIST_PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"MESSAGE_404" => "",
+		"META_DESCRIPTION" => "-",
+		"META_KEYWORDS" => "-",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"SEF_MODE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"USE_CATEGORIES" => "N",
+		"USE_FILTER" => "N",
+		"USE_PERMISSIONS" => "N",
+		"USE_RATING" => "N",
+		"USE_REVIEW" => "N",
+		"USE_RSS" => "N",
+		"USE_SEARCH" => "N",
+		"USE_SHARE" => "N",
+		"COMPONENT_TEMPLATE" => "Top_Stories",
+		"VARIABLE_ALIASES" => array(
+			"SECTION_ID" => "SECTION_ID",
+			"ELEMENT_ID" => "ELEMENT_ID",
+		)
+	),
+	false
+);?>
     <section class="foryou">
-        <div class="foryouh2">
-            <h2 id="fy">For You</h2>
-            <p>Recommended based on your interests</p>
-        </div>
-    </section>
-    <div class="foryouall">
-        <section class="foryouin">
-            <div class="row">
-                <div class="rect">
-                    <img class="bigrecimg" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/Rectangle 20.png" alt="">
-                    <div class="bigrecttxt">
-                        <h2>Sed fringilla mauris sit amet nibh.</h2>
-                        <p>Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit
-                            eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                    </div>
-                    <p class="bigrectp">15 hours ago / by dawfw</p>
-                </div>
-                <div class="rect">
-                    <img class="smrectimg" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/Rectangle 24.png" alt="">
-                    <div>
-                        <div class="smrecttxt">
-                            <h2>Sed fringilla mauris sit amet nibh.</h2>
-                            <p>Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor,
-                                suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                        </div>
-                        <p class="smrectp">15 hours ago / by dawfw</p>
-                    </div>
-                </div>
-                <div class="rect">
-                    <img class="smrectimg" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/Rectangle 25.png" alt="">
-                    <div>
-                        <div class="smrecttxt">
-                            <h2>Sed fringilla mauris sit amet nibh.</h2>
-                            <p>Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor,
-                                suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                        </div>
-                        <p class="smrectp">15 hours ago / by dawfw</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="rect">
-                    <img class="smrectimg" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/image 4.png" alt="">
-                    <div>
-                        <div class="smrecttxt">
-                            <h2>Sed fringilla mauris sit amet nibh.</h2>
-                            <p>Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor,
-                                suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                        </div>
-                        <p class="smrectp">15 hours ago / by dawfw</p>
-                    </div>
-                </div>
-                <div class="rect">
-                    <img class="smrectimg" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/Rectangle 27.png" alt="">
-                    <div>
-                        <div class="smrecttxt">
-                            <h2>Sed fringilla mauris sit amet nibh.</h2>
-                            <p>Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor,
-                                suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                        </div>
-                        <p class="smrectp">15 hours ago / by dawfw</p>
-                    </div>
-                </div>
-                <div class="rect">
-                <img class="bigrecimg" src="<?=SITE_TEMPLATE_PATH?>/assets/resources/Rectangle 26.png" alt="">
-                <div class="bigrecttxt">
-                    <h2>Sed fringilla mauris sit amet nibh.</h2>
-                    <p>Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit
-                        eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                </div>
-                <p class="bigrectp">15 hours ago / by dawfw</p>
-            </div>
-            </div>
-        </section>
-    </div>
-    <section class="back">
-        <h1 id="back"><a href="">Back</a></h1>
-    </section>
-    <div class="modal-window hidden">
-        <button class="btn--close-modal-window">&times;</button>
-        <h2 class="modal__header">
-            Login
-        </h2>
-        <form class="modal__form">
-
-            <label>Email</label>
-            <input type="email"/>
-            <label>Номер телефона</label>
-            <input type="text" class="phone"/>
-            <label for="Password">Пароль:</label>
-            <input type="password" id="Password" placeholder="Пароль">
-            <button class="btn">Далее &rarr;</button>
-        </form>
-    </div>
-    <div class="overlay hidden"></div>
-
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<div class="foryouh2">
+	<h2 id="fy">For You</h2>
+	<p>
+		 Recommended based on your interests
+	</p>
+</div>
+ </section>
+<div class="foryouall">
+ <section class="foryouin">
+	<div class="row">
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 20.png" class="bigrecimg" alt="">
+			<div class="bigrecttxt">
+				<h2>Sed fringilla mauris sit amet nibh.</h2>
+				<p>
+					 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+				</p>
+			</div>
+			<p class="bigrectp">
+				 15 hours ago / by dawfw
+			</p>
+		</div>
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 24.png" class="smrectimg" alt="">
+			<div>
+				<div class="smrecttxt">
+					<h2>Sed fringilla mauris sit amet nibh.</h2>
+					<p>
+						 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+					</p>
+				</div>
+				<p class="smrectp">
+					 15 hours ago / by dawfw
+				</p>
+			</div>
+		</div>
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 25.png" class="smrectimg" alt="">
+			<div>
+				<div class="smrecttxt">
+					<h2>Sed fringilla mauris sit amet nibh.</h2>
+					<p>
+						 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+					</p>
+				</div>
+				<p class="smrectp">
+					 15 hours ago / by dawfw
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/image 4.png" class="smrectimg" alt="">
+			<div>
+				<div class="smrecttxt">
+					<h2>Sed fringilla mauris sit amet nibh.</h2>
+					<p>
+						 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+					</p>
+				</div>
+				<p class="smrectp">
+					 15 hours ago / by dawfw
+				</p>
+			</div>
+		</div>
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 27.png" class="smrectimg" alt="">
+			<div>
+				<div class="smrecttxt">
+					<h2>Sed fringilla mauris sit amet nibh.</h2>
+					<p>
+						 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+					</p>
+				</div>
+				<p class="smrectp">
+					 15 hours ago / by dawfw
+				</p>
+			</div>
+		</div>
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 26.png" class="bigrecimg" alt="">
+			<div class="bigrecttxt">
+				<h2>Sed fringilla mauris sit amet nibh.</h2>
+				<p>
+					 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+				</p>
+			</div>
+			<p class="bigrectp">
+				 15 hours ago / by dawfw
+			</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/image 4.png" class="smrectimg" alt="">
+			<div>
+				<div class="smrecttxt">
+					<h2>Sed fringilla mauris sit amet nibh.</h2>
+					<p>
+						 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+					</p>
+				</div>
+				<p class="smrectp">
+					 15 hours ago / by dawfw
+				</p>
+			</div>
+		</div>
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 27.png" class="smrectimg" alt="">
+			<div>
+				<div class="smrecttxt">
+					<h2>Sed fringilla mauris sit amet nibh.</h2>
+					<p>
+						 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+					</p>
+				</div>
+				<p class="smrectp">
+					 15 hours ago / by dawfw
+				</p>
+			</div>
+		</div>
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 26.png" class="bigrecimg" alt="">
+			<div class="bigrecttxt">
+				<h2>Sed fringilla mauris sit amet nibh.</h2>
+				<p>
+					 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+				</p>
+			</div>
+			<p class="bigrectp">
+				 15 hours ago / by dawfw
+			</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/image 4.png" class="smrectimg" alt="">
+			<div>
+				<div class="smrecttxt">
+					<h2>Sed fringilla mauris sit amet nibh.</h2>
+					<p>
+						 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+					</p>
+				</div>
+				<p class="smrectp">
+					 15 hours ago / by dawfw
+				</p>
+			</div>
+		</div>
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 27.png" class="smrectimg" alt="">
+			<div>
+				<div class="smrecttxt">
+					<h2>Sed fringilla mauris sit amet nibh.</h2>
+					<p>
+						 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+					</p>
+				</div>
+				<p class="smrectp">
+					 15 hours ago / by dawfw
+				</p>
+			</div>
+		</div>
+		<div class="rect">
+ <img src="/local/templates/furniture_pink/assets/resources/Rectangle 26.png" class="bigrecimg" alt="">
+			<div class="bigrecttxt">
+				<h2>Sed fringilla mauris sit amet nibh.</h2>
+				<p>
+					 Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.
+				</p>
+			</div>
+			<p class="bigrectp">
+				 15 hours ago / by dawfw
+			</p>
+		</div>
+	</div>
+ </section>
+</div>
+ <section class="back">
+<h1 id="back"><a href="">Back</a></h1>
+ </section>
+<div class="modal-window hidden">
+ <button class="btn--close-modal-window">×</button>
+	<h2 class="modal__header">
+	Login </h2>
+	<form class="modal__form">
+ <label>Email</label> <input type="email"> <label>Номер телефона</label> <input type="text" class="phone"> <label for="Password">Пароль:</label> <input type="password" id="Password" placeholder="Пароль"> <button class="btn">Далее →</button>
+	</form>
+</div>
+<div class="overlay hidden">
+</div>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
